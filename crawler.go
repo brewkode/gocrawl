ea19b1691b4662a49fe9032ec26fc73a199dda05
Sriram <sriram@indix.com>
2019-02-12 21:50:08 +0530

    Refactored all functions to take input & output channels as params

--- crawler.go
+++ crawler.go
@@ -5,62 +5,28 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"time"
-	"math/rand"
+	//"time"
+	//"math/rand"
 )
 
-func urlFilter(input chan Url, output chan Url) {
-	alreadySeen := make(map[string]bool)
-	for url := range input {
-		resolvedUrl := url.GetUrl()
-		if _, isPresent := alreadySeen[resolvedUrl]; !isPresent {
-			alreadySeen[resolvedUrl] = true
-			output <- url
-		}
-	}
-}
-
-func fetchUrl(url string, out chan struct {x, y string}) {
-	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-	fmt.Printf("Fetching url %q\n", url)
-	out <- struct {x, y string} {url, "html page of url: " + url}
-	return
-}
-
-func processHtml(url string, html string, out chan string) {
-	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-	fmt.Printf("Got %d urls from html of %q\n", 1, url)
-	out <- url
-	return
-}
-
 func main() {
+	BUFFER_SIZE := 1024
 	homePage := "www.example.com"
-	urlInput := make(chan string)
-	htmlOutput := make(chan struct {x, y string})
-	extractedLinks := make(chan string)	
+	urlInput := make(chan Url, BUFFER_SIZE)
+	toCrawl := make(chan Url)
+	htmlOutput := make(chan Url)
+	// extractedLinks := make(chan Url)
 	
 	// Seeding
-	urlInput <- homePage
-
 	go func() {
 		for i := 0; i < 100; i++ {
-			urlInput <- homePage + "/" + strconv.Itoa(i)
+			urlInput <- Url{url: homePage + "/" + strconv.Itoa(i % 100)}
 		}
 	}()
 
-	for {
-		select {
-		case url := <-urlInput:
-			go fetchUrl(url, htmlOutput)
-		case out := <-htmlOutput:
-			go processHtml(out.x, out.y, extractedLinks)
-		case outLink := <- extractedLinks:
-			time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
-			fmt.Printf("Outlink %q\n", outLink)
-		default:
-		}
-	}
+	go urlFilter(urlInput, toCrawl)
+	go fetcher(toCrawl, htmlOutput)
+	go linkExtractor(htmlOutput, urlInput)
 
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
