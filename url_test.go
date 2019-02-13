e801479df62118a4f6dc8fc3cc73ef9d49ff8888
Sriram <sriram@indix.com>
2019-02-13 22:53:49 +0530

    Tests for Url helper methods

--- url_test.go
+++ url_test.go
@@ -38,3 +38,60 @@ func TestOutLinks(t *testing.T) {
       }
    }
 }
+
+func TestIsSameHost(t *testing.T) {
+   test_cases := []struct {
+        left, right string
+	same bool
+   }{
+     {"https://www.abc.com", "http://www.abc.com/testpage", true},
+     {"https://www.abc.com", "../../testpage", true},
+     {"https://www.abc.com", "http://www.abc123.com/testpage", false},
+   }
+
+   for i, c := range test_cases {
+      got := IsSameHost(c.left, c.right)
+      if got != c.same {
+          t.Errorf("Case(%d): IsSameHost(%q, %q) == %t, want %t", i, c.left, c.right, got, c.same)
+      }
+   }
+}
+
+
+func TestResolveUrl(t *testing.T) {
+   test_cases := []struct {
+        left, right string
+	resolved string
+   }{
+     {"https://www.abc.com", "../../testpage", "https://www.abc.com/testpage"},
+     {"https://www.abc.com/dir1/dir2/index.html", "../testpage", "https://www.abc.com/dir1/testpage"},
+     {"https://www.abc.com", "http://www.abc123.com/testpage", "http://www.abc123.com/testpage"},
+     {"", "http://www.abc123.com/testpage", "http://www.abc123.com/testpage"},
+   }
+
+   for i, c := range test_cases {
+      got := ResolveUrl(c.left, c.right)
+      if got != c.resolved {
+          t.Errorf("Case(%d): IsSameHost(%q, %q) == %q, want %q", i, c.left, c.right, got, c.resolved)
+      }
+   }
+}
+
+
+func TestIsSuccess(t *testing.T) {
+   test_cases := []struct {
+        url UrlResponse
+	success bool
+   }{
+     {UrlResponse{url: "http://www.abc.com", status: 200}, true},
+     {UrlResponse{url: "http://www.abc.com", status: 301}, false},
+     {UrlResponse{url: "http://www.abc.com", status: 302, redirectedUrl: "http://www.abc123.com/"}, true},
+   }
+
+   for i, c := range test_cases {
+      got := c.url.IsSuccess()
+      if got != c.success {
+          t.Errorf("Case(%d): TestIsSuccess(%q) == %t, want %t", i, c.url, got, c.success)
+      }
+   }
+}
